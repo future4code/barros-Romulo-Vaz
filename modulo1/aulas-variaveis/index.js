b1b53dd65495e0819edce63d9c198709899dd7e0
Rômulo Gabriel Silva Vaz <tdromulovaz@gmail.com>
2022-04-12 23:07:16 -0300

    exercicio-vscode

---
+++ modulo1/aulas-variaveis/index.js
@@ -0,0 +1,88 @@
+// comentario em linha unica
+
+/* comentario em 
+mais de uma linha */
+
+//comando console.log() imprimir "Olá Mundo"
+
+//console.log("Olá Mundo")
+
+// comando prompt faz uma solicitação ao usuario(abre um pop.up)
+
+//prompt( "qual seu nome")
+
+
+// VARIAVEIS
+
+//const não muda ela é CONSTANTE
+  // const     novaVariavel    =    100
+//   ^            ^          ^     ^
+// declaração    nome  atribuição  valor
+
+// let idade = 20 //let pode ser ALTERADO
+//idade = 21
+
+//console.log( idade )
+
+//const nome = "yellow"
+//const Idade = 21
+
+//console.log( "Olá" , "meu nome é" , nome , "e eu tenho" , Idade , "anos")
+
+//TIPO
+// strings: palavars, caracters, numeros
+// number: 123; 1,2; 3/5
+// booleans: true or false
+
+//let souBooleano = true
+//console.log( souBooleano)
+
+/*let nome = "Rômulo"
+let sobrenome = prompt( "seu sobrenome?")
+let idade = 20
+let estudante = true*/
+
+//console.log( nome , sobrenome , idade , estudante)
+
+// TYPEOF
+
+//console.log( typeof nome )
+ 
+//UNDEFINED OR NULL
+
+/*let minhaVariavel 
+console.log(typeof minhaVariavel )*/
+
+/*let nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+console.log(typeof nome , typeof idade)*/
+
+// CONVERTER STRING PARA NUMBER E NUMBER PARA STRING
+
+const idadeNumero = 21
+const idadeTexto = idadeNumero.toString()
+
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+
+
+const anosTexto = "21"
+const anosNumero = Number(anosTexto)
+
+console.log(typeof anosTexto)
+console.log(typeof anosNumero)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
